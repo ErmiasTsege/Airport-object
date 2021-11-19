5459b7529eb5513471c0f603988afe7ffc00ef73
Tsege <tsegeer@ad.vzwcorp.com>
2021-11-19 14:14:21 -0600

    First Commit

---
+++ bag.test.js
@@ -0,0 +1,8 @@
+const Bag= require('./bag.js')
+
+describe ("Bag Class", ()=>{ 
+    test("bag needs weight",()=> {
+        const testbag= new Bag(13)
+        expect(testbag.weight).toBe(13)
+    })
+})
